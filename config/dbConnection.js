96bf32a85fd7fedf83df14a3ec64c42ad0600396
Gleds3000 <gleds3000@gmail.com>
2020-04-29 00:01:59 -0300

    Update dbConnection.js

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -4,10 +4,14 @@ var mysql = require('mysql');
 var connMySQL = function () {
     console.log('Conexao com o bd foi estabelecida');
     return mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: '1234',
-        database: 'portal_noticias'
+       host:'CLEARDB_DATABASE_URL',
+       user: 'DB_USER',
+        password: 'DB_PASS',
+        database:'DB_NAME' 
+        // host: 'localhost',
+       // user: 'root',
+       // password: '1234',
+       // database: 'portal_noticias'
 
     });
 }
